1ffa47d26de4868bd8de1689cdb132867af84616
vini-torino <vini.torino.lab@gmail.com>
2020-02-17 07:08:05 -0300

    fixed magic key

--- parse_to_xml.py
+++ parse_to_xml.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # - * - encode: utf-8  - * -
 import sys
 import re
@@ -12,14 +12,14 @@ with open(file_path, 'r') as f:
      f.close()
 
 new_line = []
-new_line.append(f'<{wrapper}>')
+new_line.append(f'<{wrapper}>\n')
 for line in lines:
     #guardian
     line = line.rstrip()
     if len(line) <= 0 or re.findall('^#', line):
         continue
-    new_line.append(f'\t<{line[:line.index("=")].lower()} {line}>')
-new_line.append(f'</{wrapper}>')
+    new_line.append(f'\t<{line[:line.index("=")].lower()} {line}>\n')
+new_line.append(f'</{wrapper}>\n')
 
 with open(ofile, 'w') as f: 
     for line in new_line:
