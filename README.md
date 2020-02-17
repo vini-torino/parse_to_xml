20ac943334b129eb34477a7c19582d07965de19b
Vinicius Torino <vini.torino.lab@gmail.com>
2020-02-17 06:54:56 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+parse_to_xml.py
+
+Easily convert linux config files to xml format
+
+Usage:
+
+	./parse_to_xml.py /etc/default/keyboard
+
+	./parse_to_xml.py /etc/default/grub
+
+
---
+++ parse_to_xml.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# - * - encode: utf-8  - * -
+import sys
+import re
+
+file_path = sys.argv[1]
+wrapper = file_path.split('/')[-1]
+ofile = wrapper + '.xml'
+
+with open(file_path, 'r') as f:
+     lines = f.readlines()
+     f.close()
+
+new_line = []
+new_line.append(f'<{wrapper}>\n')
+for line in lines:
+    #guardian
+    line = line.rstrip()
+    if len(line) <= 0 or re.findall('^#', line):
+        continue
+    new_line.append(f'\t<{line[:line.index("=")].lower()} {line}>\n')
+new_line.append(f'</{wrapper}>\n')
+
+with open(ofile, 'w') as f: 
+    for line in new_line:
+        f.write(line)
+    f.close()
